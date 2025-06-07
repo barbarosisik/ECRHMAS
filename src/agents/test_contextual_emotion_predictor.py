a30bc34354bf3464fc03b7b33eb0caeabb8c52a0
Barbaros IŞIK <57237644+barbarosisik@users.noreply.github.com>
2025-06-07 15:01:09 +0200

    Update test_contextual_emotion_predictor.py

--- src/agents/test_contextual_emotion_predictor.py
+++ src/agents/test_contextual_emotion_predictor.py
@@ -1,17 +1,17 @@
 from contextual_emotion_predictor_agent import ContextualEmotionPredictor
 
-# Model checkpoint directory (adjust if needed)
+#model checkpoint directory
 MODEL_PATH = "/data/s3905993/ECRMAS/ECR-main/src_emo/data/saved/emp_conv2025-04-01-10-57-46/"
 LABEL_LIST = ['happy', 'nostalgia', 'negative', 'neutral', 'like', 'curious', 'grateful', 'agreement', 'surprise']
 
 predictor = ContextualEmotionPredictor(
     model_path=MODEL_PATH,
     label_list=LABEL_LIST,
-    context_turns=3,   # use last 3 turns
+    context_turns=3,   #using last 3 turns
     debug=True
 )
 
-# Example test dialogue
+#example test dialogue
 dialogue = [
     "System: What kind of movies do you like?",
     "User: I love old movies, especially ones from my childhood.",
