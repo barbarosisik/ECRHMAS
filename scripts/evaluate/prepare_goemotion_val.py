059c8592d060da175908066a9401f1a3fa4bdac2
Barbaros IŞIK <57237644+barbarosisik@users.noreply.github.com>
2025-06-07 14:50:33 +0200

    Update prepare_goemotion_val.py

--- scripts/evaluate/prepare_goemotion_val.py
+++ scripts/evaluate/prepare_goemotion_val.py
@@ -1,13 +1,13 @@
 from datasets import load_dataset
 import json
 
-# Load GoEmotions validation split
+#GoEmotions validation split
 dataset = load_dataset("go_emotions", split="validation")
 
-# Get ClassLabel object
+#ClassLabel object
 class_labels = dataset.features['labels'].feature
 
-# Prepare samples (first label only for single-label eval)
+#preparing samples
 samples = []
 for example in dataset:
     if example['labels']:
@@ -15,7 +15,7 @@ for example in dataset:
         label_name = class_labels.int2str(label_id)
         samples.append({'text': example['text'], 'label': label_name})
 
-# Save as JSON
+#saving
 with open("emotion_val_set.json", "w") as f:
     json.dump(samples, f, ensure_ascii=False, indent=2)
 
